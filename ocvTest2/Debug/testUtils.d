d24fab15adf2ad35495ac8cafcae6fe8badf407a
E16 - D <teamEDDIS@gmail.com>
2014-03-07 13:46:55 -0500

    Moved files around

--- ocvTest2/Debug/testUtils.d
+++
@@ -1,8 +0,0 @@
-testUtils.d: ../testUtils.cpp ../testUtils.h /usr/include/opencv/cv.h \
- /usr/include/opencv/highgui.h
-
-../testUtils.h:
-
-/usr/include/opencv/cv.h:
-
-/usr/include/opencv/highgui.h:
