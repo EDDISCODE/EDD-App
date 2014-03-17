2ddafda79c3fe2b46a0cae2f21ffae9305f118fd
onecan <cgs@hitchhiker.icsd.k12.ny.us>
2014-03-17 13:50:10 +0000

    Need to test things. ELI GOT TABLET WORKINGgit add -A .

--- edd/TemplateMatcher.cpp
+++ edd/TemplateMatcher.cpp
@@ -7,7 +7,7 @@
 
 #include "TemplateMatcher.h"
 
-TemplateMatcher::TemplateMatcher(string targetPath, Mat* target) {
+TemplateMatcher::TemplateMatcher(string targetPath, Mat* target, Size templSize) {
 	m_out.data = NULL;
 	m_templ.data = NULL;
 	startScale = 1;
@@ -19,33 +19,36 @@ TemplateMatcher::TemplateMatcher(string targetPath, Mat* target) {
 		m_target.data = NULL;
 }
 
-TemplateMatcher::TemplateMatcher(Mat templ, string targetPath, Mat* target) {
-	TemplateMatcher(targetPath, target);
+TemplateMatcher::TemplateMatcher(Mat templ, string targetPath, Mat* target, Size templSize) {
+	TemplateMatcher(targetPath, target, templSize);
 	setTempl(templ);
 }
 
-TemplateMatcher::TemplateMatcher(string path, string targetPath, Mat* target) {
-	TemplateMatcher(targetPath, target);
+TemplateMatcher::TemplateMatcher(string path, string targetPath, Mat* target, Size templSize) {
+	TemplateMatcher(targetPath, target, templSize);
 	setTempl(path);
 }
 
 void TemplateMatcher::setTempl(string path) {
 	m_templ = imread(path, 1);
+	resize(m_templ, m_templ, m_templSize);
 }
 
 void TemplateMatcher::setTempl(Mat templ) {
 	m_templ = templ;
+	resize(m_templ, m_templ, m_templSize);
 }
 
-void TemplateMatcher::templSize(Size sz) {
-	resize(m_templ, m_templ, sz);
-}
 
 void TemplateMatcher::run() {
 	matchTemplate(m_target, m_templ, m_out, CV_TM_SQDIFF);
-	minMaxLoc(m_out, 0, m_min, m_max);
+	minMaxLoc(m_out, NULL, NULL, m_min, m_max);
 }
 
 Point TemplateMatcher::getMin() {
-	return m_min;
+	return *m_min;
+}
+
+Point TemplateMatcher::getMax() {
+	return *m_max;
 }
--- edd/TemplateMatcher.h
+++ edd/TemplateMatcher.h
@@ -15,18 +15,20 @@ private:
 	Mat m_templ;
 	Mat m_out;
 	Mat m_target;
-	Point m_max;
-	Point m_min;
+	Point* m_max;
+	Point* m_min;
 	const int m_METHOD = 0;
 	float startScale;
+	Size m_templSize;
+
 public:
-	TemplateMatcher(string targetPath = NULL, Mat* target = NULL);
-	TemplateMatcher(Mat templ, string targetPath = NULL, Mat* target = NULL);
-	TemplateMatcher(string path, string targetPath = NULL, Mat* target = NULL);
+	TemplateMatcher(string targetPath = NULL, Mat* target = NULL, Size templSize = Size(50,50));
+	TemplateMatcher(Mat templ, string targetPath = NULL, Mat* target = NULL, Size templSize= Size(50,50));
+	TemplateMatcher(string path, string targetPath = NULL, Mat* target = NULL, Size templSize= Size(50,50));
 	void setTempl(Mat templ);
 	void setTempl(string path);
-	void templSize(Size sz);
 	Point getMin();
+	Point getMax();
 	void run();
 };
 
