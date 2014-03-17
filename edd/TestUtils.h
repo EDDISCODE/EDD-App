5da963de0069ced646cce4f2209a79e01afe5235
E16-D <teamEDDIS@gmail.com>
2014-03-17 13:29:41 -0400

    Further minor formatting changes

--- edd/TestUtils.h
+++ edd/TestUtils.h
@@ -18,9 +18,10 @@ enum TestImg{ELE, E=0, ELEPHANT=0, OLD_BALL,
 			BLK16_3, BLK256_3, BLK256_1, BALL,
 			BLK256_3_2, CRECT};
 
+string testImg(TestImg);
+
 void imgInfo(const Mat&);
 void showImg(Mat img, char* wname="window", bool wait=true);
-string testImg(TestImg);
 void printpt(const Point&, int flags = 1);
 
 #endif /* TESTUTILS_H_ */
