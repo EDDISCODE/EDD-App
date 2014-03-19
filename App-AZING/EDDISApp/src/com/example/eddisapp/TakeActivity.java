7d422953f87c78bceb174338e7aa6b2fb917ffb3
Matt Kehoe <matt.a.kehoe@gmail.com>
2014-03-19 13:39:57 -0400

    Working on icon

---
+++ App-AZING/EDDISApp/src/com/example/eddisapp/TakeActivity.java
@@ -0,0 +1,11 @@
+package com.example.eddisapp;
+
+import android.app.Activity;
+import android.content.Intent;
+
+
+public class TakeActivity extends Activity {
+	public void takeActivity(){
+	Intent takeIntent = new Intent(takeIntent.ACTION_SENDTO);
+	}
+}
