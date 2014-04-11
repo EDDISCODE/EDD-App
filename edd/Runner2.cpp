405a0c130fb2bc303851ad852d655ded9491c18d
onecan <cgs@hitchhiker.icsd.k12.ny.us>
2014-04-11 13:53:25 +0000

    more or less done with rewrite, doing some minor debugging

--- edd/Runner2.cpp
+++ edd/Runner2.cpp
@@ -1,21 +1,32 @@
 #include "Runner2.h"
 
 int main() {
-	Mat target = imread("/home/cgs/school/edd/test7.jpg");
+	Mat target = imread("/home/cgs/school/edd/test1.jpg");
 	resizeTarget(target);
 	cvtColor(target, target, CV_BGR2GRAY);
 
 	vector<Rect> locs;
 	findComparator(target, locs);
+	dispLoc(target, locs);
 	Rect avgRect;
 	if(locs.size() > 1) {
-		getAvgRect(locs, avgRect);
+		avgRect = getAvgRect(locs);
 	}
 	else avgRect = locs[0];
 
+	process(target, target);
 	Mat templ = imread("/home/cgs/school/edd/testtmp1.jpg");
 	resizeTemplate(templ, avgRect);
-	process(templ, templ);
+	process(templ, templ, genBlurSize(target));
+
+	Mat1f matcherOutput;
+	Point* max;
+	matchTemplate(target, templ, matcherOutput, CV_TM_CCORR);
+	normalize(matcherOutput, matcherOutput, 1, 0, NORM_MINMAX, -1);
+	minMaxLoc(matcherOutput, NULL, NULL, NULL, max);
+	max->x += (templ.cols+1)/2;
+	max->y += (templ.rows+1)/2;
+	dispLoc(target, vector<Point>(1, *max));
 
 
 }
@@ -31,9 +42,8 @@ void resizeTarget(Mat& img) {
 		resize(img, img, Size(0,0), scaleFac, scaleFac);
 	}
 }
-
 //Locates instances of comparator item
-bool findComparator(Mat img, vector<Rect> locs, string classifierPath) {
+bool findComparator(Mat img, vector<Rect>& locs, string classifierPath) {
 	if(img.data == NULL) {
 		std::cout << "IMAGE DATA == NULL";
 		return false;
@@ -48,17 +58,16 @@ bool findComparator(Mat img, vector<Rect> locs, string classifierPath) {
 	return true;
 }
 //Processing function for template and target
-void process(Mat in, Mat& out) {
+void process(Mat in, Mat& out, Size blurSize) {
 	if(in.channels() > 1)
 		cvtColor(in, in, CV_BGR2GRAY);
-	Size blurSize;
-	int xs, ys;
-	xs = in.cols / 100;
-	ys = in.rows / 100;
-	if(xs % 2 == 0) xs++;
-	if(ys % 2 == 0) ys++;
-	blurSize = Size(xs, ys);
-	GaussianBlur(in, out, blurSize,0, 0, 1);
+	if(blurSize.width <= 0 && blurSize.height <= 0)
+		blurSize = genBlurSize(in);
+	else {
+		if(blurSize.width % 2 == 0) blurSize.width++;
+		if(blurSize.height % 2 == 0) blurSize.height++;
+	}
+	GaussianBlur(in, out, blurSize, 0, 0, 1);
 	Mat x, y, x1, y1;
 	Scharr(out, x, 0, 1, 0, 1);
 	Scharr(out, y, 0, 0, 1, 1);
@@ -71,8 +80,8 @@ void process(Mat in, Mat& out) {
 	threshold(out, out, 235, 255, THRESH_TOZERO);
 }
 //Outputs a rectangle of average size
-bool getAvgRect(vector<Rect> rects, Rect& out) {
-	if(rects.size() < 1) return false;
+Rect getAvgRect(vector<Rect> rects) {
+	if(rects.size() < 1) return rects[0];
 	int width = 0;
 	int height = 0;
 	for(int i = rects.size() - 1; i >= 0; i--) {
@@ -81,13 +90,43 @@ bool getAvgRect(vector<Rect> rects, Rect& out) {
 	}
 	width /= rects.size();
 	height /= rects.size();
-	out = Rect();
+	Rect out = Rect();
 	out.height = height;
 	out.width = width;
-	return true;
+	return out;
 
 }
 //Resize template according to target size
-void resizeTemplate(Mat& templ, Rect comparator) {
+void resizeTemplate(Mat& templ, Rect comparator, double scaleX, double scaleY) {
+	Size sz = comparator.size();
+	sz.width *= scaleX;
+	sz.height *= scaleY;
+	resize(templ, templ, sz);
+}
+//Generates a blur size
+Size genBlurSize(Mat& img) {
+	Size sz = Size(img.cols / 100, img.rows / 100);
+	if(sz.width % 2 == 0) sz.width++;
+	if(sz.height % 2 == 0) sz.height++;
+	return sz;
+}
 
+void dispLoc(Mat img, vector<Point> points) {
+	if(img.channels() == 1)
+		cvtColor(img, img, CV_GRAY2BGR);
+	for(int i = 0; i < points.size(); i++)
+		circle(img, points[i], 25, Scalar(0, 0, 255), 5);
+	showImg(img);
+}
+
+void dispLoc(Mat img, vector<Rect> rects) {
+	if(img.channels() == 1)
+		cvtColor(img, img, CV_GRAY2BGR);
+	for(int i = 0; i < rects.size(); i++){
+		Rect r = rects[i];
+		Point p1 = Point(r.x + r.width, r.y + r.height);
+		Point p2 = Point(r.x, r.y);
+		rectangle(img, p1, p2, Scalar(0,0,255), 2);
+	}
+	showImg(img);
 }
--- edd/Runner2.h
+++ edd/Runner2.h
@@ -12,10 +12,14 @@
 #include "TestUtils.h"
 using namespace cv;
 
+
 void resizeTarget(Mat& img);
-bool findComparator(Mat img, vector<Rect> locs,
+bool findComparator(Mat img, vector<Rect>& locs,
 					string classifierPath = "/home/cgs/code/edd/haarcascade_frontalface_alt2.xml");
-void process(Mat in, Mat& out);
-bool getAvgRect(vector<Rect> rects, Rect& out);
-void resizeTemplate(Mat& templ, Rect comparator);
+void process(Mat in, Mat& out, Size blurSize = Size(0,0));
+Rect getAvgRect(vector<Rect> rects);
+void resizeTemplate(Mat& templ, Rect comparator, double scaleX = 8.6/13.0, double scaleY = 5.8 / 18.0);
+Size genBlurSize(Mat& img);
+void dispLoc(Mat img, vector<Point> points);
+void dispLoc(Mat img, vector<Rect> rects);
 #endif /* RUNNER2_H_ */
